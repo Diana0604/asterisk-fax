ec970be78db611d21e24e55e7c0724d2d2ec8815
Diana0604 <diana.vallverdu@gmail.com>
2020-08-20 14:45:44 +0100

    python reorder

--- music.py
+++
@@ -1,7 +0,0 @@
-import vlc
-
-player = vlc.MediaPlayer("audios/speaker/00-arrival.mp3")
-player.play()
-
-while player.is_playing:
-     pass
---
+++ python/main.py
@@ -0,0 +1,10 @@
+import music
+
+currentPath = "/fax/"
+#sounds 
+OPENING_THEME = "audios/speaker/00-arrival.mp3"
+BREATHING = "audios/speaker/01-breathing.mp3"
+
+#play first song
+music.Play(OPENING_THEME)
+music.Play(BREATHING)
