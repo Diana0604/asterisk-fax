5d9e40d795787152ae39b0e2c61dd543adb9f9b2
root <root@raspbx>
2020-08-20 12:11:26 +0100

    new reboot

--- reboot.sh
+++ reboot.sh
@@ -3,6 +3,11 @@
 
 #run this script from /etc/rc.local with the command sh /fax/reboot.sh
 
-/usr/bin/cvlc /fax/audios/speaker/00-arrival.mp3 > /dev/null 2>&1 & 
+#/usr/bin/cvlc /fax/audios/speaker/00-arrival.mp3 > /dev/null 2>&1 & 
 
-/usr/bin/cvlc /fax/audios/speaker/01-breathing.mp3 > /dev/null 2>&1 & 
+#/usr/bin/cvlc /fax/audios/speaker/01-breathing.mp3 > /dev/null 2>&1 & 
+
+
+python3 /fax/music.py
+
+
