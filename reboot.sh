1342d06d7fdcfc6bea77a75dbd3a8155ae157f0b
Diana0604 <diana.vallverdu@gmail.com>
2020-08-20 11:10:52 +0100

    reboot

--- reboot.sh
+++ reboot.sh
@@ -4,3 +4,5 @@
 #run this script from /etc/rc.local with the command sh /fax/reboot.sh
 
 /usr/bin/cvlc /fax/audios/speaker/00-arrival.mp3 > /dev/null 2>&1 & 
+
+/usr/bin/cvlc /fax/audios/speaker/01-breathing.mp3 > /dev/null 2>&1 & 
