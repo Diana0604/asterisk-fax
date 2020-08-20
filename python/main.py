1667122c1fb16c2c3842825baa512c0ea038a469
Diana0604 <diana.vallverdu@gmail.com>
2020-08-20 15:22:10 +0100

    debug

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 #REMEMBER TO CHANGE TO /fax BEFORE GITTING
 import music
 
+DEBUG = 1
 currentPath = "/fax/"
 #sounds 
 OPENING_THEME = currentPath + "audios/speaker/00-arrival.mp3"
@@ -13,6 +14,10 @@ opening = music.Audio(OPENING_THEME)
 breathing = music.Audio(BREATHING, 1)
 
 opening.play()
+
+if(DEBUG):
+    print('Checkpoint')
+
 threads.append(breathing.play())
 
 for thread in threads:
