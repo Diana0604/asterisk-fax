949b57c4d822a0ce0546f19357c268134354a771
Diana0604 <diana.vallverdu@gmail.com>
2020-08-21 11:51:02 +0100

    first call from python!

---
+++ calls/calls.py
@@ -0,0 +1,15 @@
+from shutil import copyfile
+
+OUTGOING_PATH = './'
+CALLS_PATH = '/fax/calls/'
+
+class Call(object):
+    def __init__(self, call):
+        self.call = call
+    
+    def dial(self):
+        copyfile( CALLS_PATH + self.call, OUTGOING_PATH + self.call)
+
+
+prova = Call('00-health.call')
+prova.dial()
--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@ import sys
 # insert at 1, 0 is the script path (or '' in REPL)
 sys.path.insert(1, '/fax/python')
 
-import music, debug
+import music, debug, calls
 
 debugger = debug.Debug(1)
 currentPath = "/fax/"
@@ -26,6 +26,11 @@ debugger.log('************ OPENING THEME FINISHED ************')
 threads.append(breathing.play())
 
 debugger.log('************ MACHINE IS BREATHING ************')
+
+health = calls.Call('00-health.call')
+health.dial()
+
+debugger.log('********* LAUNCHED FIRST CALL ****************')
 
 for thread in threads:
     thread.join()
