c2d27d4dda6926a8d6b3dccac94865b7298a0208
Diana0604 <diana.vallverdu@gmail.com>
2020-08-26 13:30:02 +0100

    just audio

--- python/main.py
+++ python/main.py
@@ -15,11 +15,10 @@ BREATHING = currentPath + "audios/speaker/01-breathing.mp3"
 threads = []
 
 #play first song
-#opening = music.Audio(OPENING_THEME)
-#breathing = music.Audio(BREATHING, 1)
+opening = music.Audio(OPENING_THEME)
+breathing = music.Audio(BREATHING, 1)
 
-#opening.play()
-
+opening.play()
 
 debugger.log('************ OPENING THEME FINISHED ************')
 
@@ -34,66 +33,66 @@ debugger.log('************ MACHINE IS BREATHING ************')
 debugger.log('********* LAUNCHED FIRST CALL ****************')
 
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER THE HEALTH FAX. THIS GOES AFTER THEY SEND A FAX")
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE HEALTH FAX. THIS GOES AFTER THEY SEND A FAX")
 #healthfax = calls.Call('00-healthfax.call')
 #healthfax.dial()
 debugger.log('********* LAUNCHED HEALTHCHECK FAX ****************')
 
 #time.sleep(100)
-helloworld = calls.Call('01-helloworld.call')
+#helloworld = calls.Call('01-helloworld.call')
 #helloworld.dial()
 debugger.log('********* LAUNCHED HELLOWORLD FAX ****************')
 
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER THE FIRST TRUST EXERCISE. THIS SHOULD HAPPEN AFTER CHECKLIST IS SENT BACK.")
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE FIRST TRUST EXERCISE. THIS SHOULD HAPPEN AFTER CHECKLIST IS SENT BACK.")
 #trust_exercise_1 = calls.Call('02-trust1.call')
 #trust_exercise_1.dial()
 debugger.log('********* LAUNCHED TRUST EX 1 ****************')
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER THE SECOND TRUST EXERCISE. THIS HSOULD HAPPEN AFTER WE KNOW THEY HAVE CALLED NUMBER XXXX.")
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE SECOND TRUST EXERCISE. THIS HSOULD HAPPEN AFTER WE KNOW THEY HAVE CALLED NUMBER XXXX.")
 #trust_exercise_2 = calls.Call('02-trust2.call')
 #trust_exercise_2.dial()
 debugger.log('********* LAUNCHED TRUST EX 2 ****************')
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER THE THIRD TRUST EXERCISE. THIS HSOULD HAPPEN AFTER WE KNOW THEY HAVE CALLED NUMBER XXXX.")
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE THIRD TRUST EXERCISE. THIS HSOULD HAPPEN AFTER WE KNOW THEY HAVE CALLED NUMBER XXXX.")
 #trust_exercise_3 = calls.Call('02-trust3.call')
 #trust_exercise_3.dial()
 debugger.log('********* LAUNCHED TRUST EX 3 ****************')
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER THE ANTENNA FAX. THIS SHOULD HAPPEN AFTER WE RECEIVE XXX FAX FROM THEM.")
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE ANTENNA FAX. THIS SHOULD HAPPEN AFTER WE RECEIVE XXX FAX FROM THEM.")
 #antenna = calls.Call('03-antenna.call')
 #antenna.dial()
 debugger.log('***************** LAUNCHED ANTENNA ***********')
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER THE SPIES FAX. THIS SHOULD HAPPEN AFTER WE RECEIVE XXX CALL FROM THEM.")
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE SPIES FAX. THIS SHOULD HAPPEN AFTER WE RECEIVE XXX CALL FROM THEM.")
 #spies = calls.Call('04-spies.call')
 #spies.dial()
 debugger.log('***************** LAUNCHED SPIES CALL ***********')
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER THE MISSION FAX. THIS SHOULD HAPPEN AFTER WE RECEIVE XXX CALL FROM THEM.")
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE MISSION FAX. THIS SHOULD HAPPEN AFTER WE RECEIVE XXX CALL FROM THEM.")
 #spies = calls.Call('05-mission.call')
 #spies.dial()
-#debugger.log('***************** LAUNCHED MISSION CALL ***********')
+debugger.log('***************** LAUNCHED MISSION CALL ***********')
 
-#input("PRESS ENTER IF YOU WANT TO TRIGGER CONSIGNIA FAX. THIS SHOULD COME AFTER ALLERGIES")
+input("PRESS ENTER IF YOU WANT TO TRIGGER CONSIGNIA FAX. THIS SHOULD COME AFTER ALLERGIES")
 #consignia = calls.Call('06-consignia.call')
 #consignia.dial()
 debugger.log('********************* LAUNCHED CONSIGNIA CALL ***************')
 
 input("PRESS WHEN LAST FAXES SHOULD COME THROUGH")
-calls.Call('07-laststeps1.call').dial()
-debugger.log('********************* LAUNCHED FINAL STEPS EXPLAINED ***************')
-time.sleep(120)
-calls.Call('07-laststeps2.call').dial()
+#calls.Call('07-laststeps1.call').dial()
+#debugger.log('********************* LAUNCHED FINAL STEPS EXPLAINED ***************')
+#time.sleep(360)
+#calls.Call('07-laststeps2.call').dial()
 debugger.log('********************* LAUNCHED FINAL STEP 1 ***************')
-time.sleep(120)
-calls.Call('07-laststeps3.call').dial()
+#time.sleep(120)
+#calls.Call('07-laststeps3.call').dial()
 debugger.log('********************* LAUNCHED FINAL STEP 2 ***************')
-time.sleep(120)
-calls.Call('07-laststeps4.call').dial()
+#time.sleep(300)
+#calls.Call('07-laststeps4.call').dial()
 debugger.log('********************* LAUNCHED FINAL STEP 3 ***************')
-time.sleep(120)
-calls.Call('07-laststeps5.call').dial()
+#time.sleep(600)
+#calls.Call('07-laststeps5.call').dial()
 debugger.log('********************* LAUNCHED FINAL STEP 4 ***************')
 
 for thread in threads:
