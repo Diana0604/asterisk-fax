418e45575253260ae4f1e3e5e4a950118a4f2818
Diana0604 <diana.vallverdu@gmail.com>
2020-08-26 11:25:41 +0100

    trust exercise 3

--- python/main.py
+++ python/main.py
@@ -45,13 +45,24 @@ helloworld = calls.Call('01-helloworld.call')
 debugger.log('********* LAUNCHED HELLOWORLD FAX ****************')
 
 
-input("PRESS ENTER IF YOU WANT TO TRIGGER THE FIRST TRUST EXERCISE. THIS SHOULD HAPPEN AFTER CHECKLIST IS SENT BACK.")
-trust_exercise_1 = calls.Call('02-trust1.call')
-trust_exercise_1.dial()
+#input("PRESS ENTER IF YOU WANT TO TRIGGER THE FIRST TRUST EXERCISE. THIS SHOULD HAPPEN AFTER CHECKLIST IS SENT BACK.")
+#trust_exercise_1 = calls.Call('02-trust1.call')
+#trust_exercise_1.dial()
+debugger.log('********* LAUNCHED TRUST EX 1 ****************')
+
+#input("PRESS ENTER IF YOU WANT TO TRIGGER THE SECOND TRUST EXERCISE. THIS HSOULD HAPPEN AFTER WE KNOW THEY HAVE CALLED NUMBER XXXX.")
+#trust_exercise_2 = calls.Call('02-trust2.call')
+#trust_exercise_2.dial()
+debugger.log('********* LAUNCHED TRUST EX 2 ****************')
 
 input("PRESS ENTER IF YOU WANT TO TRIGGER THE SECOND TRUST EXERCISE. THIS HSOULD HAPPEN AFTER WE KNOW THEY HAVE CALLED NUMBER XXXX.")
-trust_exercise_2 = calls.Call('02-trust2.call')
-trust_exercise_2.dial()
+trust_exercise_3 = calls.Call('03-trust3.call')
+trust_exercise_3.dial()
+debugger.log('********* LAUNCHED TRUST EX 3 ****************')
+
+input("PRESS ENTER IF YOU WANT TO TRIGGER THE SECOND TRUST EXERCISE. THIS SHOULD HAPPEN AFTER WE RECEIVE XXX FAX FROM THEM.")
+
+
 
 for thread in threads:
     thread.join()
