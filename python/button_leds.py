b452cbb89ff731d3f2a73b5299e90b51de7cd610
Diana0604 <diana.vallverdu@gmail.com>
2020-08-26 14:43:24 +0100

    button leds

--- python/button_leds.py
+++ python/button_leds.py
@@ -5,6 +5,7 @@ from gpiozero import PWMLED
 from gpiozero import Button
 from signal import pause
 from time import sleep
+from random import randrange
 
 ledR = PWMLED(17)
 ledG = PWMLED(27)
@@ -13,9 +14,14 @@ ledB = PWMLED(22)
 button = Button(23)
 
 def turn_lights_on():
-        ledR.value = 0.5
-        ledG.value = 0.7
-        ledB.value = 0.1
+        for i in range (0,3):
+                ledR.value = 0.5 + randrange(-2,3)
+                ledG.value = 0.5 + randrange(-2,3)
+                ledB.value = 0.5 + randrange(-2,3)
+                sleep(1)
+                ledR.value = 0
+                ledG.value = 0
+                ledB.value = 0
 
 def turn_lights_off():
         ledR.value = 0
