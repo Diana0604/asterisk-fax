09bcd5b24024a5051f03f7c22d5e05e82ebbdfa2
Diana0604 <diana.vallverdu@gmail.com>
2020-08-26 10:56:23 +0100

    trust exercise call files fix

--- python/main.py
+++ python/main.py
@@ -46,11 +46,11 @@ debugger.log('********* LAUNCHED HELLOWORLD FAX ****************')
 
 
 debugger.checkpoint("PRESS ENTER IF YOU WANT TO TRIGGER THE FIRST TRUST EXERCISE. THIS SHOULD HAPPEN AFTER CHECKLIST IS SENT BACK.")
-trust_exercise_1 = calls.Call('02-trust1')
+trust_exercise_1 = calls.Call('02-trust1.call')
 trust_exercise_1.dial()
 
 debugger.checkpoint("PRESS ENTER IF YOU WANT TO TRIGGER THE SECOND TRUST EXERCISE. THIS HSOULD HAPPEN AFTER WE KNOW THEY HAVE CALLED NUMBER XXXX.")
-trust_exercise_2 = calls.Call('02-trust2')
+trust_exercise_2 = calls.Call('02-trust2.call')
 trust_exercise_2.dial()
 
 for thread in threads:
