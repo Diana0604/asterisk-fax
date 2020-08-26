41075a09857a908ade3b4f946ecce839c4506fa2
root <root@raspbx>
2020-08-26 14:51:42 +0100

    button leds

--- python/button_leds.py
+++ python/button_leds.py
@@ -14,22 +14,26 @@ ledB = PWMLED(22)
 button = Button(23)
 
 def turn_lights_on():
-        for i in range (0,3):
-                ledR.value = 0.5 + randrange(-2,3)
-                ledG.value = 0.5 + randrange(-2,3)
-                ledB.value = 0.5 + randrange(-2,3)
-                sleep(1)
+        for i in range (0,10):
+                ledR.value = 0.5 + randrange(-2,3)*0.1
+                ledG.value = 0.5 + randrange(-2,3)*0.1
+                ledB.value = 0.5 + randrange(-2,3)*0.1
+                sleep(0.1 + randrange(0,4)*0.1)
                 ledR.value = 0
                 ledG.value = 0
                 ledB.value = 0
+		sleep(0.3)
+		sleep(0.1 + randrange(0,2)*0.1)
 
 def turn_lights_off():
         ledR.value = 0
         ledG.value = 0
         ledB.value = 0
 
-button.when_pressed = turn_lights_on
-button.when_released = turn_lights_off
+#button.when_pressed = turn_lights_on
+#button.when_released = turn_lights_off
+
+turn_lights_on()
 
 pause()
 
