1084a0e323cb5541b1678adfb01db6460c34d808
Diana0604 <diana.vallverdu@gmail.com>
2020-08-26 16:13:55 +0100

    lights as thread

--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@ import sys, time
 #insert path to system
 sys.path.insert(1, '/fax/python')
 
-import music, debug, calls#, lights
+import music, debug, calls, lights
 
 debugger = debug.Debug(1)
 currentPath = "/fax/"
@@ -19,7 +19,7 @@ threads = []
 #play first song
 
 debugger.log('************ TURNING LIGHTS ************')
-#lights.Lights().iradescent()
+lights.Lights().iradescent()
 debugger.log('************ LIGHTS ON ************')
 debugger.log('************ STARTING OPENING THEME ************')
 music.Audio(OPENING_THEME).play()
