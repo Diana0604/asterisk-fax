e8ac2a05e2572c428a7415812e21fd445bcb25ac
Diana0604 <diana.vallverdu@gmail.com>
2020-08-26 15:49:44 +0100

    python -> python3

--- reload.sh
+++ reload.sh
@@ -1,10 +1,10 @@
 cp extensions_custom.conf /etc/asterisk/extensions_custom.conf
 asterisk -rx 'dialplan reload'
 
-sudo apt-get install sendemail git python-pip
+sudo apt-get install sendemail git python3-pip
 
 sudo apt autoremove
 
-pip install gpiozero
+pip3 install gpiozero
 
 #TODO: AUDIOS
