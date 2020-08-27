69580e86ff3701dfeade4388c53d5c4546ccee84
Diana0604 <diana.vallverdu@gmail.com>
2020-08-27 13:07:04 +0100

    lits ultraviolet

--- python/lights.py
+++ python/lights.py
@@ -29,9 +29,9 @@ class Lights(object):
         finalB = 5
         while time_slept < 17:
                 ratio = time_slept*0.01
-                ledR.value = finalR*0.1 + randrange(-finalR,finalR)*0.1 #*ratio
-#                ledG.value = finalG*0.1 + randrange(-finalG,finalG)*0.1*ratio
-                ledB.value = finalB*0.1 + randrange(-finalB,finalB)*0.1 #*ratio
+                ledR.value = min(1,max(0,finalR*0.1 + randrange(-finalR,finalR)*0.1)) #*ratio
+                ledG.value = min(1,max(0,finalG*0.1 + randrange(-finalG,finalG)*0.1)) #*ratio
+                ledB.value = min(1,max(0,finalB*0.1 + randrange(-finalB,finalB)*0.1)) #*ratio
                 time1 = 0.1 + randrange(0,4)*0.1
                 time_slept += time1
                 sleep(time1)
@@ -53,14 +53,28 @@ class Lights(object):
         finalB = 0.0
         finalG = 0.0
         for i in range (0,7):
-            ledR.value = ledR.value + (finalR-initR)/7
-            ledB.value = ledB.value + (finalB-initB)/7
-            ledG.value = ledG.value + (finalG-initG)/7
+            ledR.value = min(1,max(0,ledR.value + (finalR-initR)/7))
+            ledB.value = min(1,max(0,ledB.value + (finalB-initB)/7))
+            ledG.value = min(1,max(0,ledG.value + (finalG-initG)/7))
             sleep(1)
-        ledR.value = 1
-        ledB.value = 0
-        ledG.value = 0
-
+        ledR.value = finalR
+        ledB.value = finalB
+        ledG.value = finalG
+    def purple(self):
+        initR = ledR.value
+        initG = ledG.value
+        initB = ledB.value
+        finalR = 0.7
+        finalG = 0
+        finalB = 0.3
+        for i in range (0,7):
+            ledR.value = min(1,max(0,ledR.value + (finalR-initR)/7))
+            ledG.value = min(1,max(0,ledG.value + (finalG-initG)/7))
+            ledB.value = min(1,max(0,ledB.value + (finalB-initB)/7))
+            sleep(1)
+        ledR.value = finalR
+        ledG.value = finalG
+        ledB.value = finalB
 
 #button.when_pressed = turn_lights_on
 #button.when_released = turn_lights_off
