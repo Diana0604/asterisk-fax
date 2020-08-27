77f7a4bfe53900583f09b4477305ddda9e0a2fb5
Diana0604 <diana.vallverdu@gmail.com>
2020-08-27 15:00:26 +0100

    reboot

--- reload.sh
+++ reload.sh
@@ -8,8 +8,8 @@ pip3 install gpiozero
 
 #AUDIOS
 mkdir /var/lib/asterisk/sounds/healthcheck
-cp audios/fax/01.wav /var/lib/asterisk/sounds/healthcheck/
-cp audios/fax/02.wav /var/lib/asterisk/sounds/healthcheck/
+cp audios/fax/01.mp3 /var/lib/asterisk/sounds/healthcheck/
+cp audios/fax/02.mp3 /var/lib/asterisk/sounds/healthcheck/
 
 #asterisk 
 cp extensions_custom.conf /etc/asterisk/extensions_custom.conf
