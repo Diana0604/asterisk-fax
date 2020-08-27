19bf602b19615a9ec106dadbcf51342ac695d963
Diana0604 <diana.vallverdu@gmail.com>
2020-08-27 14:58:30 +0100

    small changes

--- reload.sh
+++ reload.sh
@@ -1,10 +1,16 @@
-cp extensions_custom.conf /etc/asterisk/extensions_custom.conf
-asterisk -rx 'dialplan reload'
 
+#installations
 sudo apt-get install sendemail git python python-pip
 
 sudo apt autoremove
 
 pip3 install gpiozero
 
-#TODO: AUDIOS
+#AUDIOS
+mkdir /var/lib/asterisk/sounds/healthcheck
+cp audios/fax/01.wav /var/lib/asterisk/sounds/healthcheck/
+cp audios/fax/02.wav /var/lib/asterisk/sounds/healthcheck/
+
+#asterisk 
+cp extensions_custom.conf /etc/asterisk/extensions_custom.conf
+asterisk -rx 'dialplan reload'
