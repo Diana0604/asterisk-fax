5f1cd7c71e603eba0b74dac65c7be8d2bdef84a6
Diana0604 <diana.vallverdu@gmail.com>
2020-08-31 18:01:34 +0100

    reboot scripts

---
+++ init/email.sh
@@ -0,0 +1,7 @@
+while ! test -f "/tty-share.log"; 
+do 
+echo "no file" 
+done
+echo "file"
+
+sendemail -f diana.vallverdu@gmail.com -t diana.vallverdu@gmail.com -m "tty" -a /tty-share.log  -u 'hello' -xu diana.vallverdu@gmail.com -xp fcnxcntclkxrrxvd -s smtp.gmail.com
---
+++ init/reboot.sh
@@ -0,0 +1,26 @@
+
+#!/bin/sh
+
+#run this script from /etc/rc.local with the command sh /fax/reboot.sh
+
+#/usr/bin/cvlc /fax/audios/speaker/00-arrival.mp3 > /dev/null 2>&1 & 
+
+#/usr/bin/cvlc /fax/audios/speaker/01-breathing.mp3 > /dev/null 2>&1 & 
+
+amixer set 'Headphone' 100%
+
+#python /fax/python/main.py
+#initial vs main
+
+wget -q --spider http://google.com 
+while ! [ $? -eq 0 ] 
+do 
+echo "Offline" 
+done
+echo "Online"
+
+rm /tty-share.log
+
+sh /fax/init/email.sh &
+
+/tty-share/tty-share.rpi -logfile /tty-share.log
