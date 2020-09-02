2289951e71d8e24c96a4e4af7aadde3e01e722a7
Diana0604 <diana.vallverdu@gmail.com>
2020-09-03 00:18:12 +0100

    using playsound for music!

--- python/music.py
+++ python/music.py
@@ -1,5 +1,7 @@
 import time, vlc
 import multiprocessing
+#from pygame import mixer
+from playsound import playsound
 
 class Audio(object):
     def __init__(self, sound, background = False, looping = False, volume = 100):
@@ -15,20 +17,9 @@ class Audio(object):
             return process
         else:
             self.run()
-    def play_once(self):
-        self.player.play()
-        time.sleep(1.5)
-        duration = self.player.get_length() / 1000
-        for i in range(duration):
-            time.sleep(1)
+
     def run(self):
-        vlc_instance = vlc.Instance()
-        player = vlc_instance.media_player_new()
-        media = vlc_instance.media_new(self.sound)
-        player.set_media(media)
-        player.audio_set_volume(self.volume)
-        self.player = player
-        self.play_once()
-        if(self.looping):
+        playsound(self.sound)
+        if self.looping:
             while True:
-                self.play_once()
+                playsound(self.sound)
--- python/proves.py
+++ python/proves.py
@@ -1,20 +1,13 @@
-import multiprocessing 
-import time 
-  
-def func(number): 
-    for i in range(1, 10): 
-        time.sleep(0.01) 
-        print('Processing ' + str(number) + ': prints ' + str(number*i)) 
-  
-# list of all processes, so that they can be killed afterwards  
-all_processes = [] 
-  
-for i in range(0, 3): 
-    process = multiprocessing.Process(target=func, args=(i,)) 
-    process.start() 
-    all_processes.append(process) 
-  
-# kill all processes after 0.03s  
-time.sleep(0.03) 
-for process in all_processes: 
-    process.terminate() 
+#from pygame import mixer
+
+from playsound import playsound
+
+playsound('audios/speaker/00-arrival.mp3')
+
+#mixer.init()
+
+#sound = mixer.Sound('audios/speaker/00-arrival.mp3')
+
+#sound.play()
+
+#raw_input()
