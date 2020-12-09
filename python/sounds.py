e8bf8185a8cb338fdf9e6b9fe8cad53f206c916b
root <root@raspbx>
2020-12-09 21:30:02 +0000

    tidy sounds

--- python/sounds.py
+++ python/sounds.py
@@ -3,10 +3,8 @@ import os, utils, vlc, asterisk
 SOUNDS_PATH = '/fax/sounds/speaker/'
 DIEGETIC_SOUNDS_PATH = SOUNDS_PATH + 'diegetic/'
 BACKGROUND_SOUNDS_PATH = SOUNDS_PATH + 'background/'
-BUTTON_SOUNDS_PATH = SOUNDS_PATH + 'button/'
 background_sounds = os.listdir(BACKGROUND_SOUNDS_PATH)
 diegetic_sounds = os.listdir(DIEGETIC_SOUNDS_PATH)
-button_sounds = os.listdir(BUTTON_SOUNDS_PATH)
 
 background_player = vlc.MediaPlayer()
 previous_step = None
@@ -14,46 +12,63 @@ previous_step = None
 diegetic_players = []
 
 def get_diegetic_sound(step):
+    #if diegetic sound has already played do not play again
     if previous_step == step:
         return None
+    #look for diegetic sound in this step
     for sound in diegetic_sounds:
         if sound.startswith(step):
             return sound
     return None
 
 def get_background_sound(step):
-    found = False
     for sound in background_sounds:
         initial_step = sound[0] + sound[1]
         last_step = sound[3]+sound[4]
+        #check if step has background sound assigned
         if initial_step <= step and last_step >= step:
-            found = True
+            #if background sound is not on return sound
             if background_player.is_playing() == 0:
                 return sound
+            #if background sound is on but has to be changed return sound
             if initial_step == step and previous_step != step:
                 return sound
-    if not found:
-        return 'silence'
-    return None
+            #correct background sound is playing -> return None
+            return None
+    #no background sound found -> silence
+    return 'silence'
             
-def get_button_sound(step):
-    for sound in button_sounds:
-        if sound.startswith(step):
-            return sound
-
-def play_sound(sound, background):
-    media = vlc.Media(sound)
-    if background: 
-        background_player.stop()
-        background_player.set_media(media)
-        background_player.play()
-        return
+def play_diegetic_sound(sound):
     asterisk.wait_fax_not_ringing()
     diegetic_player = vlc.MediaPlayer()
     diegetic_player.set_media(media)
     background_player.audio_set_volume(50)
     diegetic_player.play()
     diegetic_players.append(diegetic_player)
+
+def play_background_sound(sound):
+    media = vlc.Media(sound)
+    background_player.stop()
+    background_player.set_media(media)
+    background_player.play()
+
+def launch_sounds(step):
+    global previous_step
+    #diegetic sounds
+    diegetic_sound = get_diegetic_sound(step)
+    if diegetic_sound != None:
+        diegetic_sound = DIEGETIC_SOUNDS_PATH + diegetic_sound
+        play_diegetic_sound(sound = diegetic_sound)
+    #background sounds
+    background_sound = get_background_sound(step)
+    if background_sound != None:
+        if background_sound == 'silence':
+            background_player.stop()
+        else :
+            background_sound = BACKGROUND_SOUNDS_PATH + background_sound
+            play_background_sound(sound = background_sound)
+    #update step so we do not replay sounds
+    previous_step = step
 
 def finish_diegetic_sounds():
     global diegetic_players
@@ -63,28 +78,3 @@ def finish_diegetic_sounds():
             utils.countdown(1)
     background_player.audio_set_volume(100)
     diegetic_players = []
-
-def stop_background():
-    background_player.stop()
-
-def launch_sounds(step):
-    print('managing sound')
-    global previous_step
-    print('last step was: ' + str(previous_step))
-    diegetic_sound = get_diegetic_sound(step)
-    print('we found this diegetic sound: ' + str(diegetic_sound))
-    if diegetic_sound != None:
-        print(diegetic_sound)
-        diegetic_sound = DIEGETIC_SOUNDS_PATH + diegetic_sound
-        play_sound(sound = diegetic_sound, background = False)
-    background_sound = get_background_sound(step)
-    print('we found this background sound' + str(background_sound))
-    if background_sound != None:
-        if background_sound == 'silence':
-            stop_background()
-        else :
-            print(background_sound)
-            background_sound = BACKGROUND_SOUNDS_PATH + background_sound
-            play_sound(sound = background_sound, background = True)
-    print('updateing last step to: ' + str(step))
-    previous_step = step
