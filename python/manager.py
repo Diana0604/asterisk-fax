b81ca7faddae0c2f76ded84ba9b4fa31b54008b0
Diana0604 <diana.vallverdu@gmail.com>
2020-12-29 01:00:44 +0100

    previous step checked at beginning

--- python/manager.py
+++ python/manager.py
@@ -1,5 +1,5 @@
 import calls, asterisk, utils, sounds, alsaaudio, lights, buttons, smoke, easter_eggs, datetime, os
-alsaaudio.Mixer(control=alsaaudio.Mixer().mixer()).setvolume(100)
+alsaaudio.Mixer(control=alsaaudio.Mixer().mixer()).setvolume(60)
 
 now = datetime.datetime.now()
 ## yyyy/mm/dd/hh/mm
@@ -8,15 +8,18 @@ show_date = datetime.datetime(2020, 1, 7, 14, 30)
 if show_date > now:
     sounds.play_pre_show()
     os.system('poweroff')
+    exit()
 
 #sounds.update_database()
-easter_eggs.start_easter_eggs()
-previous_step = '00'
+previous_step = str(int(asterisk.check_current_step()) - 1)
+if len(previous_step) == 1:
+    previous_step = '0' + previous_step
+
 while True:
     current_step = asterisk.check_current_step()
     #INIT
-    calls.launch_main_call(current_step)
-    calls.launch_easter_eggs()
+    #calls.launch_main_call(current_step)
+    #calls.launch_easter_eggs()
     sounds.launch_sounds(current_step)
     lights.launch_diegetic_lights(current_step)
     smoke.launch_smoke(current_step)
@@ -26,7 +29,7 @@ while True:
     lights.finish_diegetic_lights()
     lights.launch_background_lights(current_step)
     #wait for every process to be done
-    calls.finish_main_call(current_step)
+    #calls.finish_main_call(current_step)
     sounds.finish_easter_eggs_sounds()
     sounds.finish_diegetic_sounds()
     
