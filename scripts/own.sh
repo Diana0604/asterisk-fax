d6967753505e8244dfa4ad2f1f7ec068bb911f28
Diana0604 <diana.vallverdu@gmail.com>
2020-12-30 12:47:16 +0100

    new own for diana

--- scripts/own.sh
+++ scripts/own.sh
@@ -1,9 +1,9 @@
-sudo chown -R diana: /var/{lib,log,run,spool}/asterisk /usr/lib/asterisk /etc/asterisk
- 
-sudo chmod -R 750 /var/{lib,log,run,spool}/asterisk /usr/lib/asterisk /etc/asterisk
-
 asterisk -rx 'core stop now'
 
 mv /var/lib/asterisk/astdb.sqlite3 /tmp
 
 asterisk
+
+sudo chown -R diana: /var/{lib,log,run,spool}/asterisk /usr/lib/asterisk /etc/asterisk
+ 
+sudo chmod -R 750 /var/{lib,log,run,spool}/asterisk /usr/lib/asterisk /etc/asterisk
