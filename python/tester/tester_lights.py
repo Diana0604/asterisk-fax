d2a2f9152d2b977b7ac827a6ffe392c605a3a17a
root <root@raspbx>
2021-10-26 12:08:29 +0100

    tester lights

---
+++ python/tester/tester_lights.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.insert(1, '/fax/python')
+import lights
+
+question = raw_input("Write OK if you see nothing. Write anything else otherwise. ")
+
+if question != "OK":
+    print('ERROR => LIGHTS NOT OFF WHEN THEY SHOULD BE OFF')
+    sys.exit()
+
+lights.instructions_to_lights(['constant','color',1,0,0]).start()
+
+question = raw_input("Write OK if you see red. Write anything else otherwise. ")
+
+if question != "OK":
+    print('ERROR => LIGHTS NOT TURNING RED!')
+    sys.exit()
+
+lights.instructions_to_lights(['constant','color',0,1,0]).start()
+
+question = raw_input("Write OK if you see green. Write anything else otherwise. ")
+
+if question != "OK":
+    print('ERROR => LIGHTS NOT TURNING GREEN!')
+    sys.exit()
+
+lights.instructions_to_lights(['constant','color',0,0,1]).start()
+
+question = raw_input("Write OK if you see blue. Write anything else otherwise. ")
+
+if question != "OK":
+    print('ERROR => LIGHTS NOT TURNING BLUE!')
+    sys.exit()
+
+print('======================================= LIGHTS - SUCCESS! ============================')
