80849b36352f4215aa145274c905c5b1370e31eb
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2026-06-04 10:24:12 +0100

    diegetic to 70

--- python/sounds.py
+++ python/sounds.py
@@ -11,7 +11,7 @@ previous_step = None
 background_player = vlc.MediaPlayer()
 background_player.audio_set_volume(20)
 diegetic_player = vlc.MediaPlayer()
-diegetic_player.audio_set_volume(80)
+diegetic_player.audio_set_volume(70)
 easter_egg_player = vlc.MediaPlayer()
 easter_egg_player.audio_set_volume(100)
 
