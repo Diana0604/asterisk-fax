da2837239209f3055df24063b3979d45d6e80f76
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2026-06-09 16:30:47 +0100

    prep merging for performance

--- python/merger.py
+++ python/merger.py
@@ -10,6 +10,7 @@ merged_path = "/fax/tifs"
 
 FAX_WIDTH = 1728
 
+
 def normalise_resolution(img):
     img_copy = img.copy().convert("1")
     # If vertical DPI is 98, scale up to 196 to unsquash
@@ -18,14 +19,13 @@ def normalise_resolution(img):
         new_height = img_copy.height * 2
         width = img_copy.width
         img_copy = img_copy.resize(
-            (width, new_height),
-            Image.NEAREST  # NEAREST preserves hard 1-bit edges
+            (width, new_height), Image.NEAREST  # NEAREST preserves hard 1-bit edges
         )
     return img_copy
 
 
 def merge_random_faxes():
-    print("merging")
+    # print("merging")
     files = glob.glob(f"{audience_faxes}/*.tif")
     random.shuffle(files)
     print(files)
@@ -54,8 +54,10 @@ def merge_random_faxes():
 
 def merge_loop():
     need_merging = asterisk.get_database_value("prep_feed")
-    if(need_merging == "True"):
-        merge_random_faxes()
+    if need_merging != "True":
+        return
+    merge_random_faxes()
     asterisk.add_to_database("prep_feed", "False")
 
-merge_loop()
+
+# merge_loop()
