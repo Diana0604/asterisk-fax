062e6f0a0f133d73f3f69f4700a86470d708c23b
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2026-06-09 15:48:09 +0100

    merging with pIL - test tif

---
+++ python/merger.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import random, glob
+
+# audience_faxes = "/fax/tifs/output"
+# merged_path = "/fax/tifs"
+
+audience_faxes = "./tifs/output"
+merged_path = "./tifs/merged.tif"
+
+def merge_random_faxes():
+    print("merging");
+    files = glob.glob(f"{audience_faxes}/*.tif")
+    random.shuffle(files)
+    print(files)
+
+    frames = []
+
+    for i in range(0, 5):
+        if i > len(files):
+            break
+        f = files[i]
+        img = Image.open(f)
+        for i in range(img.n_frames):
+            img.seek(i)
+            frames.append(img.copy().convert("1"))  # 1-bit, standard for fax
+
+    frames[0].save(
+        merged_path,
+        save_all=True,
+        append_images=frames[1:],
+        compression="group4",  # standard fax compression
+    )
+
+merge_random_faxes()
